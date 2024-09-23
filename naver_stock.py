caf45c4216eb9d16ca530f3ffbda4007833614b8
cozym <ddongyee99@daum.net>
2024-09-23 16:19:11 +0900

    Feat: make stock_price Func

--- naver_stock.py
+++ naver_stock.py
@@ -1,8 +1,10 @@
 import requests
 import pandas as pd
 
-pageSize, page = 50, 1
-url = f'https://m.stock.naver.com/api/index/KOSPI/price?pageSize={pageSize}&page={page}'
+def stock_price(code, page=1, pageSize=60):
+    url = f'https://m.stock.naver.com/api/index/{code}/price?pageSize={pageSize}&page={page}'
+    response = requests.get(url)
+    data = response.json()
+    return pd.DataFrame(data)[['closePrice','openPrice']]
 
-response = requests.get(url)
-print(response.text)
+print(stock_price('KOSPI'))
